c06e89c2fb9d72c2c42c5d0e780c934d3dda5c55
Kevin <kodiyankevin@gmail.com>
2026-05-30 14:41:08 -0400

    ALU completed

--- rtl/alu.sv
+++ rtl/alu.sv
@@ -1,25 +1,25 @@
 module alu (
-    input logic [32:0] a, b,
-    input logic [4:0] select,
-    output logic [32:0] result
+    input logic [31:0] a, b,
+    input logic [3:0] select,
+    output logic [31:0] result
 
 );
 
 always_comb 
     begin
         case (select)
-            4'b0000 : result = a + b;
-            4'b0001 : result = a - b;
-            4'b0010 : result = a & b;
-            4'b0011 : result = a | b;
-            4'b0100 : result = ~(a || b);
-            4'b0101 : result = a << b;
-            4'b0110 : result = a >> b;
-            4'b0111 : result = a >>> b;
-            4'b1000 : result = ($signed(a) < $signed(b)) ? 1 : 0;
-            4'b1001 : result = (a < b) ? 1 : 0;
-
-            default : result = 4'b0000;
+            4'b0000 : result = a + b; // add
+            4'b0001 : result = a - b; // sub
+            4'b0010 : result = a & b; // and
+            4'b0011 : result = a | b; // or
+            4'b0100 : result = a ^ b; // xor
+            4'b0101 : result = a << b[4:0]; // sll
+            4'b0110 : result = a >> b[4:0]; // slr
+            4'b0111 : result = a >>> b[4:0]; // sar
+            4'b1000 : result = ($signed(a) < $signed(b)) ? 32'd1 : 32'd0; // slt
+            4'b1001 : result = (a < b) ? 32'd1 : 32'd0; // sltu
+ 
+            default : result = 32'b0;
         endcase
         
     end
---
+++ tb/alu_tb.sv
@@ -0,0 +1,54 @@
+module alu_tb;
+
+    logic [31:0] a, b, result, yexpected;
+    logic [3:0] select;
+
+    alu alu_instance (
+        .a(a),
+        .b(b),
+        .select(select),
+        .result(result)
+    );
+
+    function logic [31:0] expected_alu(input logic [31:0] a, b, input logic [3:0] select);
+        case(select)
+            4'b0000 : expected_alu = a + b; // add
+            4'b0001 : expected_alu = a - b; // sub
+            4'b0010 : expected_alu = a & b; // and
+            4'b0011 : expected_alu = a | b; // or
+            4'b0100 : expected_alu = a ^ b; // xor
+            4'b0101 : expected_alu = a << b[4:0]; // sll
+            4'b0110 : expected_alu = a >> b[4:0]; // slr
+            4'b0111 : expected_alu = a >>> b[4:0]; // sar
+            4'b1000 : expected_alu = ($signed(a) < $signed(b)) ? 32'd1 : 32'd0; // slt
+            4'b1001 : expected_alu = (a < b) ? 32'd1 : 32'd0; // sltu
+ 
+            default : expected_alu = 32'b0;
+        endcase     
+    endfunction
+    
+    initial 
+        begin
+            yexpected = 0;
+            for (int i = 0; i < 16; i++) 
+                begin
+                    a = $urandom;
+                    b = $urandom;
+                    select = i;
+                    #1;
+                    yexpected = expected_alu(a, b, select);
+
+                    if(yexpected != result) 
+                        $display("Error expected: %h result: %h", yexpected, result);
+                    else
+                        $display("PASS select: %h", select);
+                end
+            $finish;
+            
+            
+
+        
+        end
+    
+
+endmodule
