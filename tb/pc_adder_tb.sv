8a3b625d3dea19b98b6a0dc28372f8bf31958a5f
Kevin <kodiyankevin@gmail.com>
2026-06-09 13:35:20 -0400

    Added seperate adder module for PC

---
+++ tb/pc_adder_tb.sv
@@ -0,0 +1,37 @@
+module pc_adder_tb;
+    logic [31:0] current_address;
+    logic [31:0] next_address;
+    int errors;
+
+    pc_adder pc_adder_instance (
+        .current_address(current_address),
+        .next_address(next_address)
+    );
+
+    task automatic check_add(
+        input logic [31:0] test_address
+        );
+        
+        current_address = test_address;
+        #1;
+        assert(next_address === test_address + 4)
+        else
+            begin
+                $display("PC adder failed Input: %h, Output %h", test_address, next_address);
+                errors++;
+            end
+    endtask
+
+    initial 
+        begin
+            errors = 0;
+
+            for (int i = 0; i < 100; i++) 
+                begin
+                    check_add($urandom);
+                end
+
+            $display("Test finsihed with %d errors", errors);
+            $finish;
+        end
+endmodule
