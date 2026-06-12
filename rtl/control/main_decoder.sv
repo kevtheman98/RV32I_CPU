714466a4c8128c95f71852127e0ae3bd86b84956
Kevin <kodiyankevin@gmail.com>
2026-06-12 13:28:12 -0400

    fixed indent for readability

--- rtl/control/main_decoder.sv
+++ rtl/control/main_decoder.sv
@@ -1,12 +1,12 @@
 module main_decoder(
     input logic [6:0] main_op_in,
     output logic reg_write_sig,
-    output logic [1:0] exten_out_sig,
+    output logic [1:0] exten_src_sig,
     output logic ALU_src_sig,
     output logic mem_write_sig,
     output logic result_sig,
-    output logic branch,
-    output logic ALU_op
+    output logic branch_sig,
+    output logic ALU_op_out
     
 );
 
@@ -16,52 +16,52 @@ always_comb begin
         // lw
         7'b000_0011 :   begin
                             reg_write_sig = 1'b1;
-                            exten_out_sig = 2'b00;
+                            exten_src_sig = 2'b00;
                             ALU_src_sig = 1'b1;
                             mem_write_sig = 1'b0;
                             result_sig = 1'b1;
-                            branch = 1'b0;
-                            ALU_op = 2'b00;
+                            branch_sig = 1'b0;
+                            ALU_op_out = 2'b00;
                         end
         // sw
         7'b010_0011 :   begin
                             reg_write_sig = 1'b0;
-                            exten_out_sig = 2'b01;
+                            exten_src_sig = 2'b01;
                             ALU_src_sig = 1'b1;
                             mem_write_sig = 1'b1;
                             result_sig = 1'bx;
-                            branch = 1'b0;
-                            ALU_op = 2'b00;
+                            branch_sig = 1'b0;
+                            ALU_op_out = 2'b00;
                         end
         // R-type 
         7'b011_0011 :   begin
                             reg_write_sig = 1'b1;
-                            exten_out_sig = 2'bxx;
+                            exten_src_sig = 2'bxx;
                             ALU_src_sig = 1'b0;
                             mem_write_sig = 1'b0;
                             result_sig = 1'b0;
-                            branch = 1'b0;
-                            ALU_op = 2'b10;
+                            branch_sig = 1'b0;
+                            ALU_op_out = 2'b10;
                         end
         // branch
         7'b110_0011 :   begin
                             reg_write_sig = 1'b0;
-                            exten_out_sig = 2'b10;
+                            exten_src_sig = 2'b10;
                             ALU_src_sig = 1'b0;
                             mem_write_sig = 1'b0;
                             result_sig = 1'bx;
-                            branch = 1'b1;
-                            ALU_op = 2'b01;
+                            branch_sig = 1'b1;
+                            ALU_op_out = 2'b01;
                         end
 
         default :   begin
                         reg_write_sig = 1'b0;
-                        exten_out_sig = 2'b00;
+                        exten_src_sig = 2'b00;
                         ALU_src_sig = 1'b0;
                         mem_write_sig = 1'b0;
                         result_sig = 1'b0;
-                        branch = 1'b0;
-                        ALU_op = 2'b00;
+                        branch_sig = 1'b0;
+                        ALU_op_out = 2'b00;
 
 
                     end
@@ -69,6 +69,5 @@ always_comb begin
 
     
 end
-
 
 endmodule
