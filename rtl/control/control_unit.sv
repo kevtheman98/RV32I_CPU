98a6d3fa95eccd579a327e0ab447b7c7a7b854ac
Kevin <kodiyankevin@gmail.com>
2026-06-12 12:34:30 -0400

    Control unit implemented

---
+++ rtl/control/control_unit.sv
@@ -0,0 +1,42 @@
+module control_unit (
+    input logic [6:0] control_op,
+    input logic [2:0] funct3,
+    input logic funct7,
+    input logic zero,
+
+
+    output logic PC_sig,
+    output logic result_sig,
+    output logic mem_write_sig,
+    output logic ALU_ctrl,
+    output logic ALU_src_sig,
+    output logic exten_src_sig,
+    output logic reg_write_sig
+);
+
+    logic [2:0] ALU_op;
+    logic branch;
+
+    main_decoder main_decoder_instance (
+        .main_op_in(control_op),
+        .reg_write_sig(reg_write_sig),
+        .exten_src_sig(exten_src_sig),
+        .ALU_src_sig(ALU_src_sig),
+        .mem_write_sig(mem_write_sig),
+        .result_sig(result_sig),
+        .branch_sig(branch),
+        .ALU_op_out(ALU_op)
+    );
+
+    alu_decoder alu_decoder_instance (
+        .ALU_op_in(ALU_op),
+        .op_5_in(control_op[5]),
+        .funct3(funct3),
+        .funct7(funct7),
+        .ALU_ctrl(ALU_ctrl)
+    );
+
+    assign PC_sig = branch & zero;
+
+
+endmodule
