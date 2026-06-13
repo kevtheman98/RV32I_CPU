e7cc5af24975873f8261c3a9eb91a6946012162b
Kevin <kodiyankevin@gmail.com>
2026-06-13 17:32:44 -0400

    read from test data instead of hardcoded

--- rtl/datapath/instr_mem.sv
+++ rtl/datapath/instr_mem.sv
@@ -3,14 +3,15 @@ module instr_mem(
     output logic [31:0] instruction
 );
 
-    logic [31:0] ROM [0:1023];
+    logic [31:0] mem [0:1023];
 
     
-    // temp ROM values for testing
-    assign ROM[0] = 32'h 01498933; // add s2, s3, s4
-    assign ROM[1] = 32'h 40730283; // sub t0, t1, t2
+    initial 
+        begin
+            $readmemh("testdata/instr_mem.hex", mem);
+        end
     
     // assign instruction for current address
-    assign instruction = ROM[address >> 2]; // address (0x0,0x4,0x8) but ROM is (0x0,0x1,0x2) so divide 4 called (byte adressable)
+    assign instruction = mem[address[11:2]]; // address (0x0,0x4,0x8) but ROM is (0x0,0x1,0x2) so remove [2:0], called (byte adressable)
        
 endmodule
