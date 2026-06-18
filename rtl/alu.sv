7d2505f9012e4b6f04dee60dc94787ff78bd549b
Kevin <kodiyankevin@gmail.com>
2026-06-18 13:06:18 -0400

    made it 3 bits instead of 4

--- rtl/alu.sv
+++ rtl/alu.sv
@@ -1,6 +1,6 @@
 module alu (
     input logic [31:0] a, b,
-    input logic [3:0] select,
+    input logic [2:0] select,
     output logic [31:0] result,
     output logic zeroFlag
 
@@ -9,16 +9,14 @@ module alu (
 always_comb 
     begin
         case (select)
-            4'b0000 : result = a + b; // add
-            4'b0001 : result = a - b; // sub
-            4'b0010 : result = a & b; // and
-            4'b0011 : result = a | b; // or
-            4'b0100 : result = a ^ b; // xor
-            4'b0101 : result = a << b[4:0]; // sll
-            4'b0110 : result = a >> b[4:0]; // slr
-            4'b0111 : result = a >>> b[4:0]; // sar
-            4'b1000 : result = ($signed(a) < $signed(b)) ? 32'd1 : 32'd0; // slt
-            4'b1001 : result = (a < b) ? 32'd1 : 32'd0; // sltu
+            3'b000 : result = a + b; // add
+            3'b001 : result = a - b; // sub
+            3'b010 : result = a & b; // and
+            3'b011 : result = a | b; // or
+            3'b100 : result = a ^ b; // xor
+            3'b101 : result = a << b[4:0]; // sll
+            3'b110 : result = a >> b[4:0]; // slr
+            3'b111 : result = a >>> b[4:0]; // sar
  
             default : result = 32'bx;
         endcase
