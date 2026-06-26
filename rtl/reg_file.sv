9d4351fbe5ae49d16accdf4c719a0b8a9c779218
Kevin <kodiyankevin@gmail.com>
2026-06-26 20:01:38 +0400

    removed reset

--- rtl/reg_file.sv
+++ rtl/reg_file.sv
@@ -1,5 +1,5 @@
 module reg_file(
-    input clk, reset, WE3,
+    input clk, WE3,
     input [4:0] A1, A2, A3,
     input [31:0] WD3,
     output [31:0] RD1, RD2
@@ -12,13 +12,8 @@ assign RD1 = reg_bank[A1];
 assign RD2 = reg_bank[A2];
 
 always_ff @(posedge clk) 
-    begin
-        if(reset)
-            for (int i = 0; i < 32; i++) 
-                    reg_bank[i] <= '0;
-        else
-            if(WE3 && (A3 != 0)) // x0 register is always 0 so A3 cannot be x0 register
-                reg_bank[A3] <= WD3;
-    
+    begin      
+        if(WE3 && (A3 != 0)) // x0 register is always 0 so A3 cannot be x0 register
+            reg_bank[A3] <= WD3;
     end
 endmodule
