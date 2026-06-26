d242a319b6964235bc18f3f7097b01cfaa7d8b2e
Kevin <kodiyankevin@gmail.com>
2026-06-26 20:00:29 +0400

    Improved output naming

--- rtl/alu.sv
+++ rtl/alu.sv
@@ -1,7 +1,7 @@
 module alu (
     input logic [31:0] a, b,
     input logic [2:0] select,
-    output logic [31:0] result,
+    output logic [31:0] ALUResult,
     output logic zeroFlag
 
 );
@@ -9,19 +9,19 @@ module alu (
 always_comb 
     begin
         case (select)
-            3'b000 : result = a + b; // add
-            3'b001 : result = a - b; // sub
-            3'b010 : result = a & b; // and
-            3'b011 : result = a | b; // or
-            3'b100 : result = a ^ b; // xor
-            3'b101 : result = a << b[4:0]; // sll
-            3'b110 : result = a >> b[4:0]; // slr
-            3'b111 : result = a >>> b[4:0]; // sar
+            3'b000 : ALUResult = a + b; // add
+            3'b001 : ALUResult = a - b; // sub
+            3'b010 : ALUResult = a & b; // and
+            3'b011 : ALUResult = a | b; // or
+            3'b100 : ALUResult = a ^ b; // xor
+            3'b101 : ALUResult = a << b[4:0]; // sll
+            3'b110 : ALUResult = a >> b[4:0]; // slr
+            3'b111 : ALUResult = a >>> b[4:0]; // sar
  
-            default : result = 32'bx;
+            default : ALUResult = 32'bx;
         endcase
 
-        if(result === 0)
+        if(ALUResult === 0)
             zeroFlag = 1;
         else
             zeroFlag = 0;
