be139658a03e5aa0324b32d51289da444422fc3c
Kevin <kodiyankevin@gmail.com>
2026-06-28 00:02:05 +0400

    hardwired reg 0

--- rtl/reg_file.sv
+++ rtl/reg_file.sv
@@ -10,6 +10,7 @@ logic [31:0] reg_bank [31:0]; // 32 element x 32 bit registers
 
 assign RD1 = reg_bank[A1];
 assign RD2 = reg_bank[A2];
+assign reg_bank[0] = 0; // hardwire register 0
 
 always_ff @(posedge clk) 
     begin      
