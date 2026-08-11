1118768bd2399fe4197bcf8df8b8ce0ee7a8e052
Kevin Kodiyan <kodiyankevin@gmail.com>
2026-08-11 12:35:08 -0400

    test for lw & sw

---
+++ tests/lw_sw.s
@@ -0,0 +1,18 @@
+.text
+.global _start
+_start:
+
+    addi x2, x0, 2 #0010
+    addi x3, x0, 14 #1110
+
+    sw x3, (x2) # Store 14 in mem[2] 
+    lw x6, (x2) # Store 14 in x6
+
+    beq x2, x6, pass
+
+    sw x0, 2044(x0)
+
+    pass:
+        addi x31, x0, 1
+        sw x31, 2044(x0)
+    
