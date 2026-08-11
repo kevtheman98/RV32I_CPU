a4b8cb80e311ea6b4350a127360d6323c41d7221
Kevin Kodiyan <kodiyankevin@gmail.com>
2026-08-11 12:39:25 -0400

    get_file_name functions for root file name

--- sim/tb/top_tb.sv
+++ sim/tb/top_tb.sv
@@ -12,16 +12,33 @@ module top_tb;
     initial clk = 0;
     always #5 clk = ~clk;
 
+    function automatic string get_file_name(string path);
+        int len = path.len();
+        
+        for(int i = len-1; i >= 0; i--) begin
+            if(path[i] == "/") 
+                begin
+                    return(path.substr(i+1, len-5)); // Extract the file name without extension
+                    
+                end
+        end
+
+
+        
+        return path; // return the original path if no '/' found
+    endfunction
+
+
     initial 
         begin
 
-            // GTK waveform generation
-            $dumpfile("top_tb.vcd");
-            $dumpvars(0, top_tb);
-
-
             if(!$value$plusargs("MEMFILE=%s", mem_file))
-                mem_file = "default.mem";
+                $display("No MEMFILE arg");
+
+            // GTK waveform generation
+            $dumpfile($sformatf("%s.vcd", get_file_name(mem_file)));
+            $display(get_file_name(mem_file));
+            $dumpvars(0, top_tb);
             
             $display("Using memory file: %s", mem_file);
             $readmemh(mem_file, top_instance.instr_mem_instance.mem);
